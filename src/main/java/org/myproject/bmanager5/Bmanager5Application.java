50f985ee60c1f0b004cfe1afacc25ca8dbec9ff8
Smagin Maxim Alexeevich <maxprogger@mail.ru>
2025-09-28 18:37:02 +0700

    Repository init

---
+++ src/main/java/org/myproject/bmanager5/Bmanager5Application.java
@@ -0,0 +1,13 @@
+package org.myproject.bmanager5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bmanager5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bmanager5Application.class, args);
+	}
+
+}
