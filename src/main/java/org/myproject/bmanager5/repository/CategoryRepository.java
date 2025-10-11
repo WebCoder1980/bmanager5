8d8c8137993006f774f3b0d0ddcfa458bb5c10de
Smagin Maxim Alexeevich <maxprogger@mail.ru>
2025-10-11 11:57:32 +0700

    - Revert join fetch.

--- src/main/java/org/myproject/bmanager5/repository/CategoryRepository.java
+++ src/main/java/org/myproject/bmanager5/repository/CategoryRepository.java
@@ -1,15 +1,8 @@
 package org.myproject.bmanager5.repository;
 
-import jakarta.annotation.Nonnull;
 import org.myproject.bmanager5.model.CategoryModel;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<CategoryModel, Long> {
-    @Override
-    @EntityGraph(attributePaths = {"parents", "children"})
-    @Nonnull
-    Page<CategoryModel> findAll(@Nonnull Pageable pageable);
+
 }
