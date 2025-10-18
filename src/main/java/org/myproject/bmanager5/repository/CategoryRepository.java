f2399263aae90ef3c732a44bca494e8f4bad6ad6
Smagin Maxim Alexeevich <maxprogger@mail.ru>
2025-10-18 11:53:11 +0700

    - Remove unused method on CategoryRepository.java.

--- src/main/java/org/myproject/bmanager5/repository/CategoryRepository.java
+++ src/main/java/org/myproject/bmanager5/repository/CategoryRepository.java
@@ -1,19 +1,11 @@
 package org.myproject.bmanager5.repository;
 
-import jakarta.annotation.Nonnull;
 import org.myproject.bmanager5.model.CategoryModel;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
 
 public interface CategoryRepository
         extends JpaRepository<CategoryModel, Long>,
         JpaSpecificationExecutor<CategoryModel> {
-     @Override
-     @EntityGraph(attributePaths = {"parents", "children"})
-     @Nonnull
-     List<CategoryModel> findAll(Specification<CategoryModel> spec);
+
 }
